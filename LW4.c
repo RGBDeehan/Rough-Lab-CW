5ca5d03fc9aeb78bcd941627ba25c18bee313098
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-11 10:13:51 +0600

    Update LW4.c

--- LW4.c
+++ LW4.c
@@ -1,14 +1,29 @@
-
 #include <stdio.h>
 #include <math.h>
-int main(){
-float x;
-    printf("Enter the number: ");
-    scanf("%f",x);
 
-if( sqrt(x) == 0 ){
-        printf("This is a Perfect square");
+int main() {
+    int num;
+    double sqrt_num;
+
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+
+    if (num < 0) {
+        printf("Negative numbers cannot be perfect squares.\n");
+        return 0;
     }
-else{printf("This is not a perfect square");}
-return 0;
+
+
+    sqrt_num = sqrt(num);
+
+
+    if (sqrt_num == (int)sqrt_num) {
+        printf("%d is a perfect square.\n", num);
+    } else {
+        printf("%d is not a perfect square.\n", num);
+    }
+
+    return 0;
 }
