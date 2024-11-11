d3206fd4d0cb2cff9ba6f73fcb0dd795bef9af9b
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-11 09:51:45 +0600

    Add files via upload

---
+++ LW1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+float x;
+    printf("Enter the number: ");
+    scanf("%f",x);
+
+    if ( x % 5 == 0) {
+        printf("The number is divisible by 5");
+    }
+else {printf("The number is not divisible by 5");
+}
+
+return 0;
+}
---
+++ LW4.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <math.h>
+int main(){
+float x;
+    printf("Enter the number: ");
+    scanf("%f",x);
+
+if( sqrt(x) == 0 ){
+        printf("This is a Perfect square");
+    }
+else{printf("This is not a perfect square");}
+return 0;
+}
