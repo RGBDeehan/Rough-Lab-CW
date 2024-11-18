c5dd11075a22cd84eb1fd870838ba72005a38171
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-18 09:40:09 +0600

    Update loopTJ7.c

--- loopTJ7.c
+++ loopTJ7.c
@@ -1,18 +1,22 @@
 
+
 #include <stdio.h>
 int main(){
-int x ,  c;
+int x , s, c,avg;
 printf("Input temp:");
-
+s=0;
 c=0;
-for(int i=0;i<7;i++){
+for(int i=0;i<5;i++){
   scanf("%d",&x);
-  if(x>30){
-    c++;
+  if(x>=30){
+
+    s=s+x;
+     c++;
   }
 }
-printf("%d",c);
 
 
+avg = s/c;
+printf("Average: %d\n",avg);
 return 0 ;
 }
