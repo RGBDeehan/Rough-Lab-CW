5bddd6b980002664508518b812356662738ae68e
RGBDeehan <rageebyasardeehan@gmail.com>
2024-11-25 10:03:33 +0600

    Update Prime2.c

--- Prime2.c
+++ Prime2.c
@@ -5,7 +5,7 @@ printf("Enter num");
 scanf("%d",&x);
 i=0;
 for (i=2;i<x;i++){
-    if(x%i==0){ printf("Factors: %d\n",&i);}}
+    if(x%i==0){ printf("Factors: %d\n",i);}}
     /*c++;
     } }
 if(c==0){printf("The Number is prime\n");}
