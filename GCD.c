dfc208d7dd79b2e1a171df2ac130ba7e84a91129
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-01 14:01:18 +0600

    Add files via upload

---
+++ GCD.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+int y,i,x,a,m;
+printf("Enter 1st numbers to find GCD");
+scanf("%d",&x);
+printf("Enter 2nd numbers to find GCD");
+scanf("%d",&y);
+    if (x<y){
+        m=x;}
+else{m=y;}
+
+for (i=2;i<=m;i++) {
+    if (x%i==0 && y%i==0){
+        a=i;
+        printf("GCD: %d/n",a);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+return 0;
+}
