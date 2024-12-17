a07ca5c3a05fa13cd36418477c44aecd5e63dcc1
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-17 10:14:45 +0600

    Update ARRAYTRICKY2.c

--- ARRAYTRICKY2.c
+++ ARRAYTRICKY2.c
@@ -22,3 +22,33 @@ printf("Enter 10 numbers respectively: ");
    printf("THE SD: %d\n",SD);
     return 0;
 }
+
+
+
+/* 
+#include <stdio.h>
+#include<math.h>
+int main(){
+    float d,x[5],g,avg,b,m,n,s,c,SD;
+    int i,j;
+    printf("Enter 5 numbers respectively: ");
+s=0;
+   for(i=0;i<5;i++){
+    scanf("%f",&x[i]);
+    s=s+x[i];
+   }
+   avg=s/5;b=0;d=0;g=0;
+   for(j=0;j<5;j++){
+    b=x[j]-avg;
+    m=b*b;
+    d=d+m;
+
+    }
+    g=d/5;
+    printf("Variance are: %.2f\n",g);
+   printf("AVG: %.2f\n",avg);
+   SD=sqrt(g);
+   printf("THE SD: %.2f\n",SD);
+    return 0;
+}
+*/
