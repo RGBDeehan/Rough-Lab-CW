11e3ae125a7f9d6aa20bce8a266296881e5f824e
RGBDeehan <rageebyasardeehan@gmail.com>
2024-12-17 08:17:07 +0600

    Update maxhow.c

--- maxhow.c
+++ maxhow.c
@@ -10,13 +10,11 @@ c=0;m=x[0];
 for(j=1;j<10;j++){
     if(x[j]>m){
         m=x[j];
-    }
-}
-for(b=0;b<10;b++){
-    if(x[b]==m){
         c++;
     }
 }
+
+
 printf("Max value: %d\n",m);
 printf("Max value quantity: %d\n",c);
 
