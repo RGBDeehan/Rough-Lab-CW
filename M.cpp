5db2c32608ab0e1e6b7a75ef3ec5c5913041f331
Rageeb Yasar Deehan <rageebyasardeehan@gmail.com>
2024-12-30 09:53:45 +0600

    Add files via upload

---
+++ M.cpp
@@ -0,0 +1,17 @@
+#include <iomanip>
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+int n,i;
+scanf("%d",&n);
+char x[n];
+for(i=0;i<n;i++){
+  scanf("%s",&x);
+}
+
+
+return 0;
+}
