747f70b3859130111b5b34e1a2e03ea152a6ac4c
Rageeb Yasar Deehan <rageebyasardeehan@gmail.com>
2025-01-20 10:22:47 +0600

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,6 +1,52 @@
-#include<stdio.h>
+// Find max age from a list of ages //
+
+#include <stdio.h>
 int main(){
-    printf("Fun");
+
+freopen("age.txt","r",stdin);
+freopen("ans.txt","w",stdout);
+
+int m,n,x[100],i;
+m=0;
+for (i=0;i<n;i++){
+        scanf("%d",&x[i]);
+    if(x[i]>m){
+        m=x[i];
+    }
+}
+return 0;
+}
+
+/*
+#include <stdio.h>
+
+int main() {
+    // Open the input and output files
+    freopen("age.txt", "r", stdin);
+    freopen("ans.txt", "w", stdout);
+
+    int m, n, x[100], i;
+
+    // Read the number of ages
+    scanf("%d", &n);
+
+    // Initialize m to a small number or first value in the list
+    m = -1;
+
+    // Read the ages and find the maximum
+    for (i = 0; i < n; i++) {
+        scanf("%d", &x[i]);
+        if (x[i] > m) {
+            m = x[i];
+        }
+    }
+
+    // Write the maximum age to the output file
+    fprintf(stdout, "Maximum age is: %d\n", m);
+
     return 0;
 }
+*/
+
+
 
--- Untitled4.c
+++ Untitled4.c
@@ -1,27 +1,29 @@
 
 #include <stdio.h>
-int main(){
-int n,y,a,b,c,d=0,e=0,i,j,k,s=0,s2=0;
-scanf("%d",&n);
-int x[n*6];
-for(i=1;i<=n*6;i++){
-scanf("%d",&x[i]);
-/*printf("%d\n",x[i]);*/
-}
-s2=0;s=0;
-for(j=0;j<n*6;j++){
 
-    if(x[j]%2!=0){
-        s=s+x[j];
+int main() {
+
+    freopen("age.txt", "r", stdin);
+    freopen("ans.txt", "w", stdout);
+
+    int m, n, x[100], i;
+
+
+    scanf("%d", &n);
+
+
+    m = -1;
+
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &x[i]);
+        if (x[i] > m) {
+            m = x[i];
+        }
     }
-   else {
-        s2=s2+x[j];
 
-    }
-}
-printf("%d\n",s2);
 
-printf("%d\n",s);
+    fprintf("ans.txt"stdout, "Maximum age is: %d\n", m);
 
-return 0;
+    return 0;
 }
